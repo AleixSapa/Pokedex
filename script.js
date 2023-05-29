0b9a766a8317f81a23089accd732230262d92280
Esanpons <ecs200@hotmail.com>
2023-05-29 11:42:28 +0200

    Eliminar Buton

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('searchForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita que se envíe el formulario y se recargue la página
-    var searchTerm = document.getElementById('searchInput').value;
-    console.log('Término de búsqueda:', searchTerm);
-});
