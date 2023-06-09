120222272f13a93285330b63ede9621b911ff850
Esanpons <ecs200@hotmail.com>
2023-06-09 12:13:30 +0200

    mensage evolutivo

--- script.js
+++ script.js
@@ -5,3 +5,320 @@ function Evolucion16() {
 function Evolucion32() {
     alert('Evoluciona Al Nivel 32')
 }
+
+function MostrarMensajeEvolucion(NombrePokemon) {
+    var Nivel;
+    if (NombrePokemon == 'Bulbasaur') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Ivysaur') {
+        Nivel = 32;
+    } else if (NombrePokemon == 'Venusaur') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Charmander') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Charmeleon') {
+        Nivel = 36;
+    } else if (NombrePokemon == 'Charizard') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Squirtle') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Wartortle') {
+        Nivel = 36;
+    } else if (NombrePokemon == 'Blastoise') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Caterpie') {
+        Nivel = 7;
+    } else if (NombrePokemon == 'Metapod') {
+        Nivel = 10;
+    } else if (NombrePokemon == 'Butterfree') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Weedle') {
+        Nivel = 7;
+    } else if (NombrePokemon == 'Kakuna') {
+        Nivel = 10;
+    } else if (NombrePokemon == 'Beedrill') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Pidgey') {
+        Nivel = 18;
+    } else if (NombrePokemon == 'Pidgeotto') {
+        Nivel = 36;
+    } else if (NombrePokemon == 'Pidgeot') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Rattata') {
+        Nivel = 20;
+    } else if (NombrePokemon == 'Raticate') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Spearow') {
+        Nivel = 20;
+    } else if (NombrePokemon == 'Fearow') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Ekans') {
+        Nivel = 22;
+    } else if (NombrePokemon == 'Arbok') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Pikachu') {
+        Nivel = 0; // Pikachu no evoluciona por nivel
+    } else if (NombrePokemon == 'Raichu') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Sandshrew') {
+        Nivel = 22;
+    } else if (NombrePokemon == 'Sandslash') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Nidoran♀') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Nidorina') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Nidoqueen') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Nidoran♂') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Nidorino') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Nidoking') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Clefairy') {
+        Nivel = 0; // Clefairy evoluciona mediante Piedra Lunar
+    } else if (NombrePokemon == 'Clefable') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Vulpix') {
+        Nivel = 0; // Vulpix evoluciona mediante Piedra Fuego
+    } else if (NombrePokemon == 'Ninetales') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Jigglypuff') {
+        Nivel = 0; // Jigglypuff evoluciona mediante Piedra Lunar
+    } else if (NombrePokemon == 'Wigglytuff') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Zubat') {
+        Nivel = 22;
+    } else if (NombrePokemon == 'Golbat') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Oddish') {
+        Nivel = 21;
+    } else if (NombrePokemon == 'Gloom') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Vileplume') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Paras') {
+        Nivel = 24;
+    } else if (NombrePokemon == 'Parasect') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Venonat') {
+        Nivel = 31;
+    } else if (NombrePokemon == 'Venomoth') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Diglett') {
+        Nivel = 26;
+    } else if (NombrePokemon == 'Dugtrio') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Meowth') {
+        Nivel = 28;
+    } else if (NombrePokemon == 'Persian') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Psyduck') {
+        Nivel = 33;
+    } else if (NombrePokemon == 'Golduck') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Mankey') {
+        Nivel = 28;
+    } else if (NombrePokemon == 'Primeape') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Growlithe') {
+        Nivel = 0; // Growlithe evoluciona mediante Piedra Fuego
+    } else if (NombrePokemon == 'Arcanine') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Poliwag') {
+        Nivel = 25;
+    } else if (NombrePokemon == 'Poliwhirl') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Poliwrath') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Abra') {
+        Nivel = 16;
+    } else if (NombrePokemon == 'Kadabra') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Alakazam') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Machop') {
+        Nivel = 28;
+    } else if (NombrePokemon == 'Machoke') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Machamp') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Bellsprout') {
+        Nivel = 21;
+    } else if (NombrePokemon == 'Weepinbell') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Victreebel') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Tentacool') {
+        Nivel = 30;
+    } else if (NombrePokemon == 'Tentacruel') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Geodude') {
+        Nivel = 25;
+    } else if (NombrePokemon == 'Graveler') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Golem') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Ponyta') {
+        Nivel = 40;
+    } else if (NombrePokemon == 'Rapidash') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Slowpoke') {
+        Nivel = 37;
+    } else if (NombrePokemon == 'Slowbro') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Magnemite') {
+        Nivel = 30;
+    } else if (NombrePokemon == 'Magneton') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Farfetch\'d') {
+        Nivel = 0; // Farfetch'd no tiene evolución
+    } else if (NombrePokemon == 'Doduo') {
+        Nivel = 31;
+    } else if (NombrePokemon == 'Dodrio') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Seel') {
+        Nivel = 34;
+    } else if (NombrePokemon == 'Dewgong') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Grimer') {
+        Nivel = 38;
+    } else if (NombrePokemon == 'Muk') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Shellder') {
+        Nivel = 0; // Shellder evoluciona mediante Piedra Agua
+    } else if (NombrePokemon == 'Cloyster') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Gastly') {
+        Nivel = 25;
+    } else if (NombrePokemon == 'Haunter') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Gengar') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Onix') {
+        Nivel = 0; // Onix evoluciona mediante Intercambio con Metal Coat
+    } else if (NombrePokemon == 'Drowzee') {
+        Nivel = 26;
+    } else if (NombrePokemon == 'Hypno') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Krabby') {
+        Nivel = 28;
+    } else if (NombrePokemon == 'Kingler') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Voltorb') {
+        Nivel = 30;
+    } else if (NombrePokemon == 'Electrode') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Exeggcute') {
+        Nivel = 0; // Exeggcute evoluciona mediante Piedra Hoja
+    } else if (NombrePokemon == 'Exeggutor') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Cubone') {
+        Nivel = 28;
+    } else if (NombrePokemon == 'Marowak') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Hitmonlee') {
+        Nivel = 0; // Hitmonlee evoluciona mediante Intercambio con Puño Suave
+    } else if (NombrePokemon == 'Hitmonchan') {
+        Nivel = 0; // Hitmonchan evoluciona mediante Intercambio con Puño Duro
+    } else if (NombrePokemon == 'Lickitung') {
+        Nivel = 0; // Lickitung evoluciona mediante Intercambio con Goloseta
+    } else if (NombrePokemon == 'Koffing') {
+        Nivel = 35;
+    } else if (NombrePokemon == 'Weezing') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Rhyhorn') {
+        Nivel = 42;
+    } else if (NombrePokemon == 'Rhydon') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Chansey') {
+        Nivel = 0; // Chansey evoluciona mediante Felicidad
+    } else if (NombrePokemon == 'Tangela') {
+        Nivel = 0; // Tangela evoluciona mediante Piedra Hoja
+    } else if (NombrePokemon == 'Kangaskhan') {
+        Nivel = 0; // Kangaskhan no tiene evolución
+    } else if (NombrePokemon == 'Horsea') {
+        Nivel = 32;
+    } else if (NombrePokemon == 'Seadra') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Goldeen') {
+        Nivel = 33;
+    } else if (NombrePokemon == 'Seaking') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Staryu') {
+        Nivel = 0; // Staryu evoluciona mediante Piedra Agua
+    } else if (NombrePokemon == 'Starmie') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Mr. Mime') {
+        Nivel = 0; // Mr. Mime no tiene evolución
+    } else if (NombrePokemon == 'Scyther') {
+        Nivel = 0; // Scyther evoluciona mediante Intercambio con Metal Coat
+    } else if (NombrePokemon == 'Jynx') {
+        Nivel = 0; // Jynx no tiene evolución
+    } else if (NombrePokemon == 'Electabuzz') {
+        Nivel = 0; // Electabuzz evoluciona mediante Intercambio con Electrizador
+    } else if (NombrePokemon == 'Magmar') {
+        Nivel = 0; // Magmar evoluciona mediante Intercambio con Magmatizador
+    } else if (NombrePokemon == 'Pinsir') {
+        Nivel = 0; // Pinsir no tiene evolución
+    } else if (NombrePokemon == 'Tauros') {
+        Nivel = 0; // Tauros no tiene evolución
+    } else if (NombrePokemon == 'Magikarp') {
+        Nivel = 20;
+    } else if (NombrePokemon == 'Gyarados') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Lapras') {
+        Nivel = 0; // Lapras no tiene evolución
+    } else if (NombrePokemon == 'Ditto') {
+        Nivel = 0; // Ditto no tiene evolución
+    } else if (NombrePokemon == 'Eevee') {
+        Nivel = 0; // Eevee evoluciona mediante varias formas
+    } else if (NombrePokemon == 'Vaporeon') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Jolteon') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Flareon') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Porygon') {
+        Nivel = 0; // Porygon evoluciona mediante Intercambio con Mejora
+    } else if (NombrePokemon == 'Omanyte') {
+        Nivel = 40;
+    } else if (NombrePokemon == 'Omastar') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Kabuto') {
+        Nivel = 40;
+    } else if (NombrePokemon == 'Kabutops') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Aerodactyl') {
+        Nivel = 0; // Aerodactyl no tiene evolución
+    } else if (NombrePokemon == 'Snorlax') {
+        Nivel = 0; // Snorlax no tiene evolución
+    } else if (NombrePokemon == 'Articuno') {
+        Nivel = 0; // Articuno no tiene evolución
+    } else if (NombrePokemon == 'Zapdos') {
+        Nivel = 0; // Zapdos no tiene evolución
+    } else if (NombrePokemon == 'Moltres') {
+        Nivel = 0; // Moltres no tiene evolución
+    } else if (NombrePokemon == 'Dratini') {
+        Nivel = 30;
+    } else if (NombrePokemon == 'Dragonair') {
+        Nivel = 55;
+    } else if (NombrePokemon == 'Dragonite') {
+        Nivel = 0;
+    } else if (NombrePokemon == 'Mewtwo') {
+        Nivel = 0; // Mewtwo no tiene evolución
+    } else if (NombrePokemon == 'Mew') {
+        Nivel = 0; // Mew no tiene evolución
+    } else {
+        Nivel = -1; // Si no se encuentra el Pokémon, se devuelve -1
+    }
+
+    if (Nivel > 0) {
+        console.log(NombrePokemon + ' evoluciona al nivel ' + Nivel);
+    } else if (Nivel == 0) {
+        console.log(NombrePokemon + ' no tiene más evoluciones');
+    } else {
+        console.log('No se encontró el Pokémon ' + NombrePokemon);
+    }
+}
